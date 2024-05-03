dfc21f667981b0b27c089c18f9c5396235c70969
박주원 <83098949+k-r-1@users.noreply.github.com>
2024-05-03 15:06:49 +0900

    [level 0] Title: A 강조하기, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/181874. A 강조하기/A 강조하기.py
+++ 프로그래머스/0/181874. A 강조하기/A 강조하기.py
@@ -1,10 +1,2 @@
 def solution(myString):
-    result = ''
-    for char in myString:
-        if char == 'a':
-            result += 'A'
-        elif char != 'A' and char.isupper():
-            result += char.lower()
-        else:
-            result += char
-    return result
+    return myString.lower().replace('a', 'A')
