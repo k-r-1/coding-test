ac1543f0e9a08bfad150cfe60f8f7cd4920749a7
주원 <83098949+k-r-1@users.noreply.github.com>
2025-09-29 15:06:33 +0900

    [level 0] Title: 배열 만들기 4, Time: 52.02 ms, Memory: 12.6 MB -BaekjoonHub

--- 프로그래머스/0/181918. 배열 만들기 4/배열 만들기 4.py
+++ 프로그래머스/0/181918. 배열 만들기 4/배열 만들기 4.py
@@ -2,12 +2,12 @@ def solution(arr):
     i = 0
     stk = []
     while i < len(arr):
-            if len(stk) == 0:
-                stk.append(arr[i])
-                i += 1
-            elif stk is not None and (stk[-1] < arr[i]):
-                stk.append(arr[i])
-                i += 1
-            elif stk is not None and (stk[-1] >= arr[i]):
-                stk.remove(stk[-1])
+        if not stk:
+            stk.append(arr[i])
+            i += 1
+        elif stk[-1] < arr[i]:
+            stk.append(arr[i])
+            i += 1
+        else:
+            stk.pop()
     return stk
