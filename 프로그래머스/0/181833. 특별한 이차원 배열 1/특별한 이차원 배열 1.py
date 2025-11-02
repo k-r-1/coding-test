1d5cdd0ada40fd842f717ac7bc451b0c8dd3cf19
주원 <83098949+k-r-1@users.noreply.github.com>
2025-11-03 03:07:36 +0900

    [level 0] Title: 특별한 이차원 배열 1, Time: 0.47 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/181833. 특별한 이차원 배열 1/특별한 이차원 배열 1.py
+++ 프로그래머스/0/181833. 특별한 이차원 배열 1/특별한 이차원 배열 1.py
@@ -1,2 +1,5 @@
 def solution(n):
-    return [[1 if i == j else 0 for j in range(n)] for i in range(n)]
+    return [
+        [1 if i == j else 0 for j in range(n)]
+         for i in range(n)
+    ]
