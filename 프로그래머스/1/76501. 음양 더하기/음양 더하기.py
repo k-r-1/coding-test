b49756bffec50ea031f56d01703ab9521a815202
주원 <83098949+k-r-1@users.noreply.github.com>
2026-06-02 10:57:20 +0900

    [level 1] Title: 음양 더하기, Time: 0.06 ms, Memory: 11.4 MB -BaekjoonHub

--- 프로그래머스/1/76501. 음양 더하기/음양 더하기.py
+++ 프로그래머스/1/76501. 음양 더하기/음양 더하기.py
@@ -1,10 +1,8 @@
 def solution(absolutes, signs):
     answer = 0
-    
     for num, is_positive in zip(absolutes, signs):
         if is_positive:
             answer += num
         else:
             answer -= num
-            
-    return answer 
+    return answer
